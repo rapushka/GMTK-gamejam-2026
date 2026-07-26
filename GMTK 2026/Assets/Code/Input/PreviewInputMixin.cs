1bc54b9898ad8f02c8afd102c62e7bd00e978e0e
rapuska <mikalajsiatkouski@gmail.com>
2026-07-26 17:52:05 +0300

    better rotate

--- GMTK 2026/Assets/Code/Input/PreviewInputMixin.cs
+++ GMTK 2026/Assets/Code/Input/PreviewInputMixin.cs
@@ -1,11 +1,18 @@
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Core
 {
     public class PreviewInputMixin
     {
+        private const float ClickMoveThreshold = 4f;
+
         private static ItemPreviewSystem ItemPreview  => ServiceLocator.Get<ItemPreviewSystem>();
         private static CameraSystem      CameraSystem => ServiceLocator.Get<CameraSystem>();
+
+        private Vector2 _pressPoint;
+        private bool _pressOffModel;
+        private bool _dragStarted;
 
         public void HandleInput(Mouse mouse)
         {
@@ -13,17 +20,35 @@ namespace Core
 
             if (mouse.leftButton.wasPressedThisFrame)
             {
-                if (CameraSystem.IsPointerOnPreview(mouseScreenPoint))
+                _pressPoint = mouseScreenPoint;
+                _dragStarted = false;
+                _pressOffModel = !CameraSystem.IsPointerOnPreview(mouseScreenPoint);
+
+                if (!_pressOffModel)
                     ItemPreview.StartRotate(mouseScreenPoint);
-                else
-                    ItemPreview.Hide();
 
                 return;
             }
 
             if (mouse.leftButton.isPressed)
             {
+                if (!_dragStarted && Vector2.Distance(_pressPoint, mouseScreenPoint) > ClickMoveThreshold)
+                {
+                    _dragStarted = true;
+
+                    if (_pressOffModel)
+                        ItemPreview.StartRotate(_pressPoint);
+                }
+
                 ItemPreview.Rotate(mouseScreenPoint);
+                return;
+            }
+
+            if (mouse.leftButton.wasReleasedThisFrame
+                && _pressOffModel
+                && !_dragStarted)
+            {
+                ItemPreview.Hide();
                 return;
             }
 
